2b8f340d3029db6b4077307d68833c037e08db3d
quus1 <dao329x@outlook.com>
2024-06-25 10:42:30 +0800

    Modify in.cpp

--- in.cpp
+++ in.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Economic tend per change relationship black real.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
