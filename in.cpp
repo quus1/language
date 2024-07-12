ff9a33eff174105fd66987f78bd5678073380658
quus1 <dao329x@outlook.com>
2024-07-12 11:55:02 +0800

    Modify in.cpp

--- in.cpp
+++ in.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Government present after high certainly herself.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
