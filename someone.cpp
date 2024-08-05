2eaa13f88d0b3e4911449559b05265ee50260950
quus1 <dao329x@outlook.com>
2024-08-05 22:58:39 +0800

    Modify someone.cpp

--- someone.cpp
+++ someone.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Stage through opportunity purpose.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
