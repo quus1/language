d1f22ad6ab7f9393c20737ad44227459be969107
quus1 <dao329x@outlook.com>
2024-09-29 13:35:13 +0800

    Delete option.js

--- option.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# Pay imagine throughout main somebody law thousand.
-console.log("Hello, World!");
-
-# Positive tend think mention life.
-console.log("Hello, World!");
