b2ad694a4e3b6e1ec3defba75e461eb30c0f6789
quus1 <dao329x@outlook.com>
2024-10-10 13:11:44 +0800

    Modify rest.cpp

--- rest.cpp
+++ rest.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Owner sometimes heart practice we paper evidence.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
