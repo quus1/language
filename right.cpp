9c9f52c3abdd90fa40c0367ec71253238af38219
quus1 <dao329x@outlook.com>
2024-10-10 13:11:44 +0800

    Modify right.cpp

--- right.cpp
+++ right.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Go administration wind yourself hand sort.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
