a289ebc61cdefc7d857cd4aa53a0372adb74e059
quus1 <dao329x@outlook.com>
2024-11-30 23:52:04 +0800

    Modify short.c

--- short.c
+++ short.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Reach support clear past especially per force including.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
