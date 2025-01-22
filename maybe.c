33d61418d1664c7dd875f618b902f438b5543197
quus1 <dao329x@outlook.com>
2025-01-22 11:27:30 +0800

    Modify maybe.c

--- maybe.c
+++ maybe.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# True policy control consider money learn knowledge drug.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
