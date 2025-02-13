c4a66d97c54d7ef5d364671fb5c98ecea4aeee32
quus1 <dao329x@outlook.com>
2025-02-13 15:02:36 +0800

    Modify cost.cpp

--- cost.cpp
+++ cost.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Figure suffer list artist floor prepare.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
